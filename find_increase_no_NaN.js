c981c17f0f1c4886837c82b5a13565d8fe2df42c
jhuang794 <60958731+jhuang794@users.noreply.github.com>
2020-11-25 06:11:48 -0800

    Google Sheets script for finding increase percent

---
+++ find_increase_no_NaN.js
@@ -0,0 +1,26 @@
+function findIncrease() {
+  
+  var app = SpreadsheetApp;
+  var ss = app.getActiveSpreadsheet();
+  var activeSheet = ss.getActiveSheet();
+  var i = 2;
+  
+  while(activeSheet.getRange(i, 1).getValue() === activeSheet.getRange(i+1, 1).getValue()) {
+    if(activeSheet.getRange(i, 10).isBlank() || activeSheet.getRange(i+1, 10).isBlank()) {
+      activeSheet.getRange(i, 12).setValue(0);
+      i++;
+    }
+    else {
+      activeSheet.getRange(i, 12).setValue(100*(activeSheet.getRange(i, 10).getValue() - activeSheet.getRange(i+1, 10).getValue())/(activeSheet.getRange(i+1, 10).getValue()));
+      i++;
+    }
+    if(activeSheet.getRange(i, 1).getValue() != activeSheet.getRange(i+1, 1).getValue()) {
+      if(activeSheet.getRange(i+1, 1).isBlank()) {
+        activeSheet.getRange(i, 12).setValue(0);
+        break;
+      }
+      activeSheet.getRange(i, 12).setValue(0);
+      i++;
+    }
+  }
+}
