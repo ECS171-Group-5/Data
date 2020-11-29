c812297a3e192cc348332a91c416847aeb83dc82
jhuang794 <60958731+jhuang794@users.noreply.github.com>
2020-11-28 21:50:34 -0800

    Updated increasePercent

--- find_increase.js
+++ find_increase.js
@@ -5,15 +5,15 @@ function findIncrease() {
   var activeSheet = ss.getActiveSheet();
   var i = 2;
   
+  activeSheet.getRange(2, 12).setValue(0);
   while(activeSheet.getRange(i, 1).getValue() === activeSheet.getRange(i+1, 1).getValue()) {
-    activeSheet.getRange(i, 12).setValue(100*(activeSheet.getRange(i, 10).getValue() - activeSheet.getRange(i+1, 10).getValue())/(activeSheet.getRange(i+1, 10).getValue()));
+    activeSheet.getRange(i+1, 12).setValue(100*(activeSheet.getRange(i, 10).getValue() - activeSheet.getRange(i+1, 10).getValue())/(activeSheet.getRange(i+1, 10).getValue()));
     i++;
     if(activeSheet.getRange(i, 1).getValue() != activeSheet.getRange(i+1, 1).getValue()) {
       if(activeSheet.getRange(i+1, 1).isBlank()) {
-        activeSheet.getRange(i, 12).setValue(0);
         break;
       }
-      activeSheet.getRange(i, 12).setValue(0);
+      activeSheet.getRange(i+1, 12).setValue(0);
       i++;
     }
   }
