198ea37a11fe7fdca77e73904f45a787f0b4b872
jhuang794 <60958731+jhuang794@users.noreply.github.com>
2020-11-28 21:52:02 -0800

    Updated increasePercent

--- find_increase.js
+++ find_increase.js
@@ -3,12 +3,18 @@ function findIncrease() {
   var app = SpreadsheetApp;
   var ss = app.getActiveSpreadsheet();
   var activeSheet = ss.getActiveSheet();
-  var i = 2;
+  var i = 5555;
   
   activeSheet.getRange(2, 12).setValue(0);
   while(activeSheet.getRange(i, 1).getValue() === activeSheet.getRange(i+1, 1).getValue()) {
-    activeSheet.getRange(i+1, 12).setValue(100*(activeSheet.getRange(i, 10).getValue() - activeSheet.getRange(i+1, 10).getValue())/(activeSheet.getRange(i+1, 10).getValue()));
-    i++;
+    if(activeSheet.getRange(i, 10).isBlank() || activeSheet.getRange(i+1, 10).isBlank()) {
+      activeSheet.getRange(i+1, 12).setValue(0);
+      i++;
+    }
+    else {
+      activeSheet.getRange(i+1, 12).setValue(100*(activeSheet.getRange(i, 10).getValue() - activeSheet.getRange(i+1, 10).getValue())/(activeSheet.getRange(i+1, 10).getValue()));
+      i++;
+    }
     if(activeSheet.getRange(i, 1).getValue() != activeSheet.getRange(i+1, 1).getValue()) {
       if(activeSheet.getRange(i+1, 1).isBlank()) {
         break;
